81a4fcf582c0afc4fec9c44617c23e1fccb093da
dranh101200@gmail.com <dranh101200@gmail.com>
2018-08-24 09:44:01 +0700

    fix something

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule.withServerTransition({ appId: 'angular-universal' })
   ],
   providers: [],
   bootstrap: [AppComponent]
